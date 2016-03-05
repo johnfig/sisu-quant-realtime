b541976924854e40ac17e726fe0d32a679377158
John Figueiredo <john@johnfigueiredo.com>
2016-03-05 11:04:52 -0800

    Refactor data with async api calls

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var yahooFinance = require('yahoo-finance');
 var fred = require('fred')('1ab947af701f804360c98accc8bf46ec' || process.env.FRED_API_KEY);
+var async = require("async");
 
 router.get('/', function(req, res, next) {
   fred.series.observations('UNRATE', function(err, unemploymentRate) {
@@ -10,22 +11,21 @@ router.get('/', function(req, res, next) {
     };
   });
 
-  fred.series.observations('FEDFUNDS', function(err, federalFundsRate) {
-    if (federalFundsRate) {
-      this.federalFundsRate = federalFundsRate.observations.pop().value
-    };
-  });
+  data = [
+           ['UNRATE', 'unemploymentRate'],
+           ['FEDFUNDS', 'federalFundsRate'],
+           ['CIVPART', 'laborParticipationRate'],
+           ['GFDEGDQ188S', 'debtToGDP'],
+         ]
 
-  fred.series.observations('CIVPART', function(err, laborParticipationRate) {
-    if (laborParticipationRate) {
-      this.laborParticipationRate = laborParticipationRate.observations.pop().value
-    };
-  });
-
-  fred.series.observations('GFDEGDQ188S', function(err, debtToGDP) {
-    if (debtToGDP) {
-      this.debtToGDP = debtToGDP.observations.pop().value
-    };
+  async.eachSeries(data, function(name, cb) {
+    fred.series.observations(name[0], function(err, result) {
+      if (result) {
+        console.log(result.observations.pop().value);
+        this[name[1]] = result.observations.pop().value
+      };
+      cb();
+    });
   });
 
   res.render('dashboard',{
