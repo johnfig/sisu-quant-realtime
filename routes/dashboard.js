d5c69c7b37d48219894aaaf1bb1cdc908cc81e7f
John Figueiredo <john@johnfigueiredo.com>
2016-03-05 11:15:04 -0800

    Refactor fred api calls into private method

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,6 +4,13 @@ var yahooFinance = require('yahoo-finance');
 var fred = require('fred')('1ab947af701f804360c98accc8bf46ec' || process.env.FRED_API_KEY);
 var async = require("async");
 
+var data = [
+             ['UNRATE', 'unemploymentRate'],
+             ['FEDFUNDS', 'federalFundsRate'],
+             ['CIVPART', 'laborParticipationRate'],
+             ['GFDEGDQ188S', 'debtToGDP'],
+           ]
+
 router.get('/', function(req, res, next) {
   fred.series.observations('UNRATE', function(err, unemploymentRate) {
     if (unemploymentRate) {
@@ -11,22 +18,7 @@ router.get('/', function(req, res, next) {
     };
   });
 
-  data = [
-           ['UNRATE', 'unemploymentRate'],
-           ['FEDFUNDS', 'federalFundsRate'],
-           ['CIVPART', 'laborParticipationRate'],
-           ['GFDEGDQ188S', 'debtToGDP'],
-         ]
-
-  async.eachSeries(data, function(name, cb) {
-    fred.series.observations(name[0], function(err, result) {
-      if (result) {
-        console.log(result.observations.pop().value);
-        this[name[1]] = result.observations.pop().value
-      };
-      cb();
-    });
-  });
+  setEconomicData(data)
 
   res.render('dashboard',{
     title: 'Dashboard for Sisu Quant',
@@ -36,5 +28,16 @@ router.get('/', function(req, res, next) {
     debtToGDP: this.debtToGDP,
   });
 });
+
+function setEconomicData(data) {
+  async.eachSeries(data, function(name, cb) {
+    fred.series.observations(name[0], function(err, result) {
+      if (result) {
+        this[name[1]] = result.observations.pop().value
+      };
+      cb();
+    });
+  });
+}
 
 module.exports = router;
